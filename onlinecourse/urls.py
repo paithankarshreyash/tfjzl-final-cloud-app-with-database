69f97078c2bc1ca41efcfa34819971af08624eef
paithankarshreyash <paithankarshreyash@gmail.com>
2026-03-06 12:55:57 +0530

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,24 +1,30 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
-    path('registration/', views.registration_request, name='registration'),
+    # Course list page
+    path('', views.CourseListView.as_view(), name='index'),
+
+    # User authentication
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
-    path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
-    path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    path('registration/', views.registration_request, name='registration'),
 
-    # <HINT> Create a route for submit view
+    # Course detail page
+    path('course/<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
 
-    # <HINT> Create a route for show_exam_result view
+    # Enroll in course
+    path('course/<int:course_id>/enroll/', views.enroll, name='enroll'),
 
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Submit exam
+    path('course/<int:course_id>/submit/', views.submit, name='submit'),
+
+    # Show exam result
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
+]
